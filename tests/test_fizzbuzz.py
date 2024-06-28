e7af4567b6046330f0593db744d5367f6775ee57
thigaskrl <00001106165755sp@al.educacao.sp.gov.br>
2024-06-28 09:11:36 -0300

    aula 3

---
+++ tests/test_fizzbuzz.py
@@ -0,0 +1,20 @@
+from app.app import create_app
+from app.fizzbuzz.fizzbuzz import fizzbuzz
+
+flask_app = create_app()
+
+def test_fizz():
+    res = fizzbuzz(3)
+    assert res == 'fizz'
+
+def test_buzz():
+    res = fizzbuzz(5)
+    assert res == 'buzz'
+
+def test_fizzbuzz():
+    res = fizzbuzz(15)
+    assert res == 'fizzbuzz'
+
+def test_number():
+    res = fizzbuzz(2)
+    assert res == 2
